1419dc4767a57179389c4f76583f4bfc81b4cec6
R Harshitha Prabhu <harshithaprabhu.19.1999@gmail.com>
2021-09-02 11:03:49 +0530

    All f

---
+++ app.js
@@ -0,0 +1,13 @@
+let displayArea = document.querySelector("#displayArea")
+let inputbox = document.querySelector("#inputbox")
+let mybtn = document.querySelector("#mybtn")
+
+mybtn.addEventListener('click', function(){
+
+let a=5;
+let b=8;
+let c=a+b;
+
+displayArea.innerHTML=c;
+
+})
